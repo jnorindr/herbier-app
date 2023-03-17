962fdf047c880c6b427bebbd4d2f6d91f2c41594
jnorindr <jade.norindr@gmail.com>
2023-03-17 17:42:13 +0100

    modification route erreurs

--- app/app.py
+++ app/app.py
@@ -12,4 +12,4 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 
 # Import des routes
-from .routes import generales
+from .routes import generales, erreurs
--- app/routes/erreurs.py
+++ app/routes/erreurs.py
@@ -4,11 +4,11 @@ from flask import render_template
 # Route en cas d'erreur 404 avec affichage d'un template
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('pages/error404.html'), 404
+    return render_template('pages/erreurs/error404.html'), 404
 
 # Route en cas d'erreur 500 ou 503 avec affichage d'un template
 @app.errorhandler(500)
 @app.errorhandler(503)
 def internal_error(e):
     db.session.rollback()
-    return render_template('pages/error500.html'), 500
+    return render_template('pages/erreurs/error500.html'), 500
