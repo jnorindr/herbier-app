caaf5cb0f7fb302d9aff7b447bc0f92ef68fd624
jnorindr <jade.norindr@gmail.com>
2023-03-17 17:38:30 +0100

    création routes et templates erreurs

---
+++ app/routes/erreurs.py
@@ -0,0 +1,14 @@
+from ..app import app, db
+from flask import render_template
+
+# Route en cas d'erreur 404 avec affichage d'un template
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('pages/error404.html'), 404
+
+# Route en cas d'erreur 500 ou 503 avec affichage d'un template
+@app.errorhandler(500)
+@app.errorhandler(503)
+def internal_error(e):
+    db.session.rollback()
+    return render_template('pages/error500.html'), 500
--- app/routes/generales.py
+++ app/routes/generales.py
@@ -1,5 +1,5 @@
 from ..app import app, db
-from flask import Flask, render_template
+from flask import render_template, abort, flash
 from ..models.iiif_api import IIIF
 from ..models.database import Herbier
 import json, requests
@@ -9,52 +9,59 @@ import json, requests
 
 # Définition de la fonction permettant d'identifier la plante et d'ajouter ces données à la base
 def identification(folio):
-    # Import depuis le fichier config.py de la clé d'API et du continent de travail
-    API_KEY = app.config['API_KEY']
-    PROJECT = app.config['PROJECT']
+    try:
+        # Test de l'existence de l'illustration dans la table
+        if Herbier.query.filter(Herbier.id==folio).first():
+            # Import depuis le fichier config.py de la clé d'API et du continent de travail
+            API_KEY = app.config['API_KEY']
+            PROJECT = app.config['PROJECT']
 
-    # Création de l'URL pour la requête à l'API
-    api_endpoint = f"https://my-api.plantnet.org/v2/identify/{PROJECT}?api-key={API_KEY}"
+            # Création de l'URL pour la requête à l'API
+            api_endpoint = f"https://my-api.plantnet.org/v2/identify/{PROJECT}?api-key={API_KEY}"
 
-    # Utilisation du wrapper PyGallica pour récupérer l'image, l'enregistrer et récupérer son chemin
-    # Utilisation de la variable folio pour construire l'URL pour chaque illustration
-    image = IIIF.iiif('3A%2F12148%2Fbtv1b8451620k/'+folio, 'full', 'full', '0', 'native', 'jpg')
+            # Utilisation du wrapper PyGallica pour récupérer l'image, l'enregistrer et récupérer son chemin
+            # Utilisation de la variable folio pour construire l'URL pour chaque illustration
+            image = IIIF.iiif('3A%2F12148%2Fbtv1b8451620k/'+folio, 'full', 'full', '0', 'native', 'jpg')
 
-    # Lecture en binaire de l'image enregistrée précédemment
-    image_data = open(image, 'rb')
+            # Lecture en binaire de l'image enregistrée précédemment
+            image_data = open(image, 'rb')
 
-    # Création du dictionnaire des organes des plante sur les images (systématiquement des fleurs dans notre cas)
-    data = {
-        'organs': ['flower']
-    }
+            # Création du dictionnaire des organes des plante sur les images (systématiquement des fleurs dans notre cas)
+            data = {
+               'organs': ['flower']
+            }
 
-    # Création d'une liste de tuples correspondants à l'image et son chemin pour la requête
-    files = [
-        ('images', (image, image_data))
-    ]
+            # Création d'une liste de tuples correspondants à l'image et son chemin pour la requête
+            files = [
+                ('images', (image, image_data))
+            ]
 
-    # Requête à l'API PlantNet pour l'identification à partir des fichiers image, du dictionnaire d'organes et de l'URL définis précédemment
-    req = requests.Request('POST', url=api_endpoint, files=files, data=data)
+            # Requête à l'API PlantNet pour l'identification à partir des fichiers image, du dictionnaire d'organes et de l'URL définis précédemment
+            req = requests.Request('POST', url=api_endpoint, files=files, data=data)
 
-    # Mise en forme de la réponse de l'API au format JSON
-    prepared = req.prepare()
-    s = requests.Session()
-    response = s.send(prepared)
-    ident = json.loads(response.text)
+            # Mise en forme de la réponse de l'API au format JSON
+            prepared = req.prepare()
+            s = requests.Session()
+            response = s.send(prepared)
+            ident = json.loads(response.text)
 
-    # AJOUTER CONDITION : SI PREMIÈRE FOIS
-    # Import des valeurs qui nous intéressent dans la base de données herbier à partir de la réponse en JSON de l'API
-    Herbier.query.filter(Herbier.id == folio).\
-        update({"famille": ident['results'][0]['species']['family']['scientificNameWithoutAuthor'],
+            # Import des valeurs qui nous intéressent dans la base de données herbier à partir de la réponse en JSON de l'API
+            Herbier.query.filter(Herbier.id == folio).\
+                update({"famille": ident['results'][0]['species']['family']['scientificNameWithoutAuthor'],
                 "nom_commun1": ', '.join(ident['results'][0]['species']['commonNames']),
                 "nom_latin1": ident['results'][0]['species']['scientificNameWithoutAuthor'],
                 "nom_latin2": ident['results'][1]['species']['scientificNameWithoutAuthor'],
                 "nom_commun2": ', '.join(ident['results'][1]['species']['commonNames']),
                 "nom_commun3": ', '.join(ident['results'][2]['species']['commonNames']),
                 "nom_latin3": ident['results'][2]['species']['scientificNameWithoutAuthor']})
-    db.session.commit()
+            db.session.commit()
 
-    # Requête pour les données de l'illustration dont l'identifiant dans la base est équivalent au folio
-    donnees = Herbier.query.filter(Herbier.id == folio).first()
+            # Cette route renvoie le template info_plante
+            return render_template("/pages/info_plante.html", donnees=Herbier.query.filter(Herbier.id == folio).first(), folio=folio)
 
-    return render_template("/pages/info_plante.html", donnees=donnees, folio=folio)
+        # Une erreur 404 est renvoyée si l'illustration n'existe pas
+        else:
+            abort(404)
+        # Erreur 404 en cas d'erreur
+    except Exception as e:
+        abort(404)
