9055ddc896c42b556a76feb59f44508b4a360d8a
jnorindr <jade.norindr@gmail.com>
2023-03-18 11:50:36 +0100

    ajout message flashing

--- app/routes/insertions.py
+++ app/routes/insertions.py
@@ -7,15 +7,20 @@ from ..models.formulaires import InsertionPlante
 def insertion_plante(folio):
     form = InsertionPlante()
 
-    if form.validate_on_submit():
-        famille2 = request.form.get("famille2", None)
-        nom_commun4 = request.form.get("nom_commun4", None)
-        nom_latin4 = request.form.get("nom_latin4", None)
-        commentaire = request.form.get("commentaire", None)
+    try:
+        if form.validate_on_submit():
+            famille2 = request.form.get("famille2", None)
+            nom_commun4 = request.form.get("nom_commun4", None)
+            nom_latin4 = request.form.get("nom_latin4", None)
+            commentaire = request.form.get("commentaire", None)
 
-        Herbier.query.filter(Herbier.id == folio).\
-                update({"famille2": famille2, "nom_commun4": nom_commun4, 
-                        "nom_latin4": nom_latin4, "commentaire": commentaire})
-        db.session.commit()
+            Herbier.query.filter(Herbier.id == folio).\
+                    update({"famille2": famille2, "nom_commun4": nom_commun4, 
+                            "nom_latin4": nom_latin4, "commentaire": commentaire})
+            db.session.commit()
+            flash("L'identification " + nom_latin4 + " a bien été ajoutée", "info")
+
+    except Exception as erreur:
+        flash("Une erreur s'est produite lors de l'insertion de l'identification " + nom_latin4 + " : " + str(erreur), "error")
 
     return render_template("pages/insertion_plante.html", form=form, folio=folio)
