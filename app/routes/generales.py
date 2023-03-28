716d47a3183f8160fed7b91733ca2d5343d2ba1e
jnorindr <jade.norindr@gmail.com>
2023-03-29 00:16:20 +0200

    ajout condition route info_plante, correction marges, changement bannière

--- app/routes/generales.py
+++ app/routes/generales.py
@@ -133,69 +133,76 @@ def info_plante(folio):
     try:
         # Test de l'existence de l'illustration dans la table
         if Herbier.query.filter(Herbier.id==folio).first():
-            # Import depuis le fichier config.py de la clé d'API et de la zone géographique
-            API_KEY = app.config['API_KEY']
-            PROJECT = app.config['PROJECT']
+            # Création d'une variable avec les données de la requête
+            donnees = Herbier.query.filter(Herbier.id==folio).first()
 
-            # Création de l'URL pour la requête à l'API
-            api_endpoint = f"https://my-api.plantnet.org/v2/identify/{PROJECT}?api-key={API_KEY}"
+            # Vérifier que les données n'ont pas déjà été importées pour éviter d'appeler l'API à chaque chargement de la page
+            if not donnees.famille:
+                # Import depuis le fichier config.py de la clé d'API et de la zone géographique
+                API_KEY = app.config['API_KEY']
+                PROJECT = app.config['PROJECT']
 
-            # Utilisation du wrapper PyGallica pour récupérer l'image, l'enregistrer et récupérer son chemin : utilisation de la méthode iiif de la classe IIIF
-            # Utilisation de la variable folio pour construire l'URL pour chaque illustration
-            image = IIIF.iiif('3A%2F12148%2Fbtv1b8451620k/'+folio, 'full', 'full', '0', 'native', 'jpg')
+                # Création de l'URL pour la requête à l'API
+                api_endpoint = f"https://my-api.plantnet.org/v2/identify/{PROJECT}?api-key={API_KEY}"
 
-            # Lecture en binaire de l'image enregistrée précédemment
-            image_data = open(image, 'rb')
+                # Utilisation du wrapper PyGallica pour récupérer l'image, l'enregistrer et récupérer son chemin : utilisation de la méthode iiif de la classe IIIF
+                # Utilisation de la variable folio pour construire l'URL pour chaque illustration
+                image = IIIF.iiif('3A%2F12148%2Fbtv1b8451620k/'+folio, 'full', 'full', '0', 'native', 'jpg')
 
-            # Création du dictionnaire des organes des plante sur les images (systématiquement des fleurs dans notre cas)
-            data = {
-               'organs': ['flower']
-            }
+                # Lecture en binaire de l'image enregistrée précédemment
+                image_data = open(image, 'rb')
 
-            # Création d'une liste de tuples correspondants à l'image et son chemin pour la requête
-            files = [
-                ('images', (image, image_data))
-            ]
+                # Création du dictionnaire des organes des plante sur les images (systématiquement des fleurs dans notre cas)
+                data = {
+                    'organs': ['flower']
+                }
 
-            # Requête à l'API PlantNet pour l'identification à partir des fichiers image, du dictionnaire d'organes et de l'URL définis précédemment
-            req = requests.Request('POST', url=api_endpoint, files=files, data=data)
+                # Création d'une liste de tuples correspondants à l'image et son chemin pour la requête
+                files = [
+                    ('images', (image, image_data))
+                ]
 
-            # Mise en forme de la requête sous forme de dictionnaire
-            prepared = req.prepare()
+                # Requête à l'API PlantNet pour l'identification à partir des fichiers image, du dictionnaire d'organes et de l'URL définis précédemment
+                req = requests.Request('POST', url=api_endpoint, files=files, data=data)
 
-            # Création d'une session pour conserver les paramètres au fil des différentes requêtes
-            s = requests.Session()
+                # Mise en forme de la requête sous forme de dictionnaire
+                prepared = req.prepare()
 
-            # Envoi de la requête préparée et récupération de la réponse
-            response = s.send(prepared)
+                # Création d'une session pour conserver les paramètres au fil des différentes requêtes
+                s = requests.Session()
 
-            # Transformation de la réponse au format JSON vers un dictionnaire Python
-            ident = json.loads(response.text)
+                # Envoi de la requête préparée et récupération de la réponse
+                response = s.send(prepared)
 
-            # Vérification de l'existence des résultats dans le dictionnaire
-            if 'results' in ident.keys():
-                # Import des valeurs qui nous intéressent dans la base de données herbier à partir de la réponse en JSON de l'API
-                Herbier.query.filter(Herbier.id == folio).\
-                update({"famille": ident['results'][0]['species']['family']['scientificNameWithoutAuthor'],
-                "nom_commun1": ', '.join(ident['results'][0]['species']['commonNames']),
-                "nom_latin1": ident['results'][0]['species']['scientificNameWithoutAuthor'],
-                "nom_latin2": ident['results'][1]['species']['scientificNameWithoutAuthor'],
-                "nom_commun2": ', '.join(ident['results'][1]['species']['commonNames']),
-                "nom_commun3": ', '.join(ident['results'][2]['species']['commonNames']),
-                "nom_latin3": ident['results'][2]['species']['scientificNameWithoutAuthor']})
-                db.session.commit()
+                # Transformation de la réponse au format JSON vers un dictionnaire Python
+                ident = json.loads(response.text)
+
+                # Vérification de l'existence des résultats dans le dictionnaire pour éviter erreur fatale en cas d'échec de l'identification
+                if 'results' in ident.keys():
+                    # Import des valeurs qui nous intéressent dans la base de données herbier à partir de la réponse en JSON de l'API
+                    Herbier.query.filter(Herbier.id == folio).\
+                    update({"famille": ident['results'][0]['species']['family']['scientificNameWithoutAuthor'],
+                    "nom_commun1": ', '.join(ident['results'][0]['species']['commonNames']),
+                    "nom_latin1": ident['results'][0]['species']['scientificNameWithoutAuthor'],
+                    "nom_latin2": ident['results'][1]['species']['scientificNameWithoutAuthor'],
+                    "nom_commun2": ', '.join(ident['results'][1]['species']['commonNames']),
+                    "nom_commun3": ', '.join(ident['results'][2]['species']['commonNames']),
+                    "nom_latin3": ident['results'][2]['species']['scientificNameWithoutAuthor']})
+                    db.session.commit()
             
-            # Sinon retourner un message signalant que l'identification est impossible
+                # Sinon signaler que l'identification est impossible
+                else:
+                    Herbier.query.filter(Herbier.id == folio).\
+                    update({"famille": 'Espèce inconnue'})
+
+                # Enfin, renvoyer le template info_plante.html qui utilisera comme données la requête ci-dessous
+                return render_template("/pages/info_plante.html", sous_titre=donnees.poems[0].titre, donnees=donnees, folio=folio)
+            
+            # Sinon, si l'identification est déjà faite, retourner directement le template avec les données
             else:
-                Herbier.query.filter(Herbier.id == folio).\
-                update({"famille": 'Espèce inconnue'})
-
-            # Création d'une variable associée aux données requêtées dans la base
-            donnees=Herbier.query.filter(Herbier.id == folio).first()
-
-            # Cette route renvoie le template info_plante.html qui utilisera comme données la requête ci-dessous
-            return render_template("/pages/info_plante.html", sous_titre=donnees.poems[0].titre, donnees=donnees, folio=folio)
-        
+                # Cette route renvoie le template info_plante.html qui utilisera comme données la requête ci-dessous
+                return render_template("/pages/info_plante.html", sous_titre=donnees.poems[0].titre, donnees=donnees, folio=folio)
+            
         # Une erreur 404 est renvoyée si l'illustration n'existe pas
         else:
             abort(404)
